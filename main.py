f4d782bc7d821266088d00be77793cf430539180
Jean Castillo <168604159+jeangoo@users.noreply.github.com>
2025-07-25 08:59:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 name = "Jean"
 print(name)
+
+def celsius_a_fahrenheit(celsius):
+    return (celsius * 9/5) + 32
+
+try:
+    c = float(input("Introduce la temperatura en °C: "))
+    f = celsius_a_fahrenheit(c)
+    print(f"{c} °C equivalen a {f:.2f} °F")
+except ValueError:
+    print("Por favor, introduce un número válido.")
